a215792f631629878d47580d22aedd16b4d320ae
kdm-python <99660919+kdm-python@users.noreply.github.com>
2024-06-11 14:42:29 +0100

    Add files via upload

--- pyman.py
+++ pyman.py
@@ -3,16 +3,29 @@
 """
 PyMan
 Bring up help, functions, or definition file conveniently in the command line
+
+--- TODO ---
+
+* pyman threading.Thread is not bringing up the module function help
+* Give suggestions for module functions too: e.g. thread.Thread will match search term 'Thr'
+* Find out how to find file path of module that is inbuilt (May only work when installed in venv)
+* Interative menu -i to loop over search, open man page then come back to program
+
+* Must be a way to do autocomplete on command line input: expand into full application
+
 """
+
 
 import sys
 import argparse
 import pkgutil
 import importlib
 import subprocess
-import inspect
-from typing import Any, Callable
 import webbrowser
+
+# This is a new line
+def foo():
+    print("Look")
 
 def import_module(module_name: str):  # Return type for module?
     try:
@@ -21,24 +34,10 @@ def import_module(module_name: str):  # Return type for module?
     except ImportError:
         print(f"Module {module_name} not found.")
 
-def get_module_functions(module_name: str, include_dunder: bool = False) -> list[Callable]:  # -> list[Callable]:
-    # Import should be done first and object passed to here or the man page function
-    try:
-        module = importlib.import_module(module_name)
-    except ModuleNotFoundError as err:
-        print(err)
-        sys.exit()
-    else:
-        attributes = dir(module)
-        functions = [
-            getattr(module, attr).__name__ for attr in attributes 
-            if callable(getattr(module, attr))
-            
-        ]
-        if not include_dunder:
-            functions = [func for func in functions if not func.startswith("_")]
-            
-        return functions
+def get_module_functions(module):  # -> list[Callable]:
+    attributes = dir(module)
+    functions = [attr for attr in attributes if not attr.startswith("_")]
+    return functions
 
 def get_modules() -> list:
     module_list = [x.name for x in list(pkgutil.iter_modules()) if not x.name.startswith("_")]
@@ -47,34 +46,21 @@ def get_modules() -> list:
 def get_builtins() -> list:
     return [x for x in dir(__builtins__) if not x.startswith("_")]
 
-def open_module_man(module):
-    help(module)
+def print_man(func_obj):
+    eval("print(help(func_obj))")
 
-def open_module_func(func_obj):
-    help(func_obj)
-
-def open_man_page(module: Any, function: Callable | None = None) -> None:
-    # Required parameter: first command line arg => man of module
-    # Optional second arg after module ot trigger running 
-    # Type for module import object?
-    if not function:
-        help(module)
-    else:
-        module_function_string = f"help({module}.{function})"
-        print(f"{module_function_string=}")
-        print("TODO: Access module function definition")
-
-def get_source_path():
-    # Only open ones with a .py file located
-    # Builtin ones written in C can be ignored for the moment
-    # Display message if a file not found: inform the builtin is in the interpreter in C
-    ...
-
-def open_file_source(func_obj: Callable, editor="vim"):
-    # Run get_source_path to find file, if one found, do this:
+def open_definition(func_obj, editor="vim"):
+    # Locate file path where function is
+    # May not work for builtins
     subprocess.run(["echo", f"TODO: find file where the definition for {func_obj} is and open in {editor}"])
-    # if get_source_path(func_obj( is None: echo a message
     exit(0)
+
+def create_eval_string(module: str, function: str | None = None):
+    if not function:
+        eval_string = f"help({module})"
+    else:
+        eval_string = f"help({module}.{function})"
+    return eval_string
 
 def partial_search(search_term: str) -> list[str]:
     """
@@ -91,8 +77,10 @@ def search_all(search_term: str) -> str | None:
     mods = get_modules()
     builtin = get_builtins()
     if search_term in mods:
+        print(f"'{search_term}' found in module list.")
         return "module"
     elif search_term in builtin:
+        print(f"'{search_term}' found in builtin function list.")
         return "builtin"
     if len(partial_search(search_term)) > 0:
         print("Possible suggestions:")
@@ -102,19 +90,14 @@ def search_all(search_term: str) -> str | None:
         return "partial"
 
 def open_docs_page(module_name: str) -> None:
-    """
-    --- TODO ---
-    * Run same check suggestions here like with man function
-    * Check if 404 not found, then if this response, don't open in browser:
-    Will have to check the http status code somehow?
-    * Jump to highlighted definition of a single function within the docs: 
-    Run ctrl-f in the browser somehow?
-    """
     url = f"https://docs.python.org/3/library/{module_name}.html"
+    # If invalid URL, print no page found
+    # Idea: Jump to a function definition on the page
     try:
         webbrowser.open(url)
     except Exception as e:
         print(e)
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -135,12 +118,6 @@ def main():
     )
     
     parser.add_argument(
-        "-m", "--man", 
-        help="Open module's man page in the console.",
-        action="store_true"
-    )
-    
-    parser.add_argument(
         "-d", "--dir", 
         help="List available functions and classes for this module",
         action="store_true"
@@ -154,27 +131,14 @@ def main():
     
     parser.add_argument(
         "-doc", "--documentation", 
-        help="Open official docs page in browser",
+        help="TODO: Open official docs page in browser",
         action="store_true"
     )
     
     args = parser.parse_args()
-    if args.documentation is True:
-        open_docs_page(args.module)
-        sys.exit()
-    if args.dir is True:
-        print(f"--- Inbuilt Functions for <{args.module}> ---")
-        for module in get_module_functions(args.module):
-            print(f"* {module}")
-        sys.exit()
 
-    if args.definition is True:
-        open_file_source(args.module)
-    
-    # No other arguments, default to --man
     results = search_all(args.module)
     if not results:
-        print("No matches found.")
         sys.exit() 
     elif results == "module":
         mod = import_module(args.module)
@@ -183,17 +147,14 @@ def main():
             for f in funcs:
                 print(f)
             sys.exit(0)
-        open_module_man(mod)
+        print_man(mod)
     
     elif results == "builtin":
         # str is not caught here it should be treated as a module
-        open_module_man(args.module)
+        print_man(args.module)
     elif results == "partial":
-        # Prints any close matches in above function
+        # str is not caught here it should be treated as a module
         pass
-
-if __name__ == "__main__":
-    main()
 
 def run_menu_loop(module_name: str, search_filter: str | None = None):
     """
@@ -215,6 +176,16 @@ def run_menu_loop(module_name: str, search_filter: str | None = None):
     func_objects = get_module_functions(mod)
     print(f"{func_objects[:10]=}")
     
+    # TODO: List of function dicts [{index, func_name, func_obj}]
+    # Then access the function object or name based on index, run man function
+    # assert choice in list of dicts index keys
+    
+    test_indexes = [1, 2, 3]
+    test_func_names = [ "importlib", "argparse", "sys"]
+    test_func_objs = [importlib, argparse, sys]
+    # Iterate above with keys and values somehow
+    test_func_dicts = [{}, {}, {}]
+
     while True:
         print(f"--- Functions and Classes for {module_name} ---")
         # Loop over list of dicts func_name
@@ -222,19 +193,24 @@ def run_menu_loop(module_name: str, search_filter: str | None = None):
             print(f"{i}: {func}")
         choice = input("Enter number or q to exit -> ")
         if choice == ("q" or "Q"):
-            sys.exit()
+            sys.exit(0)
         else:
             # Loop over list of dicts func_obj I think
             for i, func in enumerate(dir(mod)):
                 if i == "t":
                     # Access func obj based on the displayed numbers
                     test_func_obj = sys.modules
-                    open_module_man(test_func_obj)
+                    print_man(test_func_obj)
                     # man_process = subprocess.Popen(['man', function_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                     continue
-                elif choice == "i":
+                elif choice == i:
                     print("TODO: Match choice to index of function")
                     continue
                 print(f"{choice} is invalid.")
                     # Now to wait until man processed funished in shell, return to loop
                     # Perhaps something in subprocess module. Or maybe does automatically?
+
+if __name__ == "__main__":
+    main()
+    # m = partial_search("th")
+    # print(m)
